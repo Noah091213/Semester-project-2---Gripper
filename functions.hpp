d65e63be004a9554395939e6bf56382c7162bdc0
domigit314 <schlabberkrak@gmail.com>
2025-05-15 14:35:55 +0200

    removed database functions from functions.hpp

--- functions.hpp
+++ functions.hpp
@@ -1,17 +1,8 @@
 #ifndef FUNCTIONS_HPP
 #define FUNCTIONS_HPP
-#include <sqlite3.h>
 
 void setPinInput(unsigned int name);    // Function to set up an input GPIO pin
 void setPinOutput(unsigned int name);   // Function to set up an output GPIO pin
 void runTest(int toTest);               // Function to test pins due to the lack of error messages from library
-
-//Database functions:
-int displayData(void *placeholder, int totalColumns, char **values, char **columnName); //Internal function to display the data. Used internally in displayTable.
-void openDatabase();                                                                    //Opens the database.
-void createTable();                                                                     //Creates a table if it does not already exist.
-void insertData(bool succesfullGrip);                                                    //Inserts data into table.
-void displayTable();                                                                    //Displayes the table.
-void closeDatabase();                                                                   //Closes the database.
 
 #endif
